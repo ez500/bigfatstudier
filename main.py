e6878b79dc637f1efb2c329ba018fcfd2819cf6e
bigfatmidget <eugenezhang500@gmail.com>
2023-03-06 23:35:00 -0500

    bug fixes (e.g. capitalization + 'all' keyword)

--- main.py
+++ main.py
@@ -10,39 +10,41 @@ with open('homework_tasks', 'r') as f:
     subject = ast.literal_eval(f.read())
 
 
-def check_subject(subject_name):
+def get_real_subject(subject_name):
     for i in subject:
-        if i == subject_name:
-            return True
-    return False
+        if i.lower() == subject_name.lower():
+            return i
+    raise KeyError('This subject doesn\'t exist!')
 
 
 def get_subject_homework(subject_name):
     for i in subject:
-        if i == subject_name:
-            return subject[subject_name]
+        if i.lower() == subject_name.lower():
+            return subject[i]
     raise KeyError('This subject doesn\'t exist!')
 
 
 def set_subject_homework(subject_name, assignment):
     for i in subject:
-        if i == subject_name:
-            subject[subject_name] = assignment
+        if i.lower() == subject_name.lower():
+            subject[i] = assignment
             return
     raise KeyError('This subject doesn\'t exist!')
 
 
 def add_subject(subject_name):
     for i in subject:
-        if i == subject_name:
+        if i.lower() == subject_name.lower():
             raise KeyError('This subject is already added!')
+        elif subject_name.lower() == 'all':
+            raise NameError('You can\'t add an \'all\' subject!')
     subject[subject_name] = 'None'
 
 
 def remove_subject(subject_name):
     for i in subject:
-        if i == subject_name:
-            subject.pop(subject_name)
+        if i.lower() == subject_name.lower():
+            subject.pop(i)
             return
     raise KeyError('This subject never existed!')
 
@@ -93,14 +95,18 @@ async def subjects(ctx, options=None, *, subject_name=None):
                 add_subject(subject_name)
                 await ctx.send(f'Successfully added {subject_name} to the subject list!')
             except KeyError:
-                await ctx.send(f'{subject_name} already exists!')
+                real_subject = get_real_subject(subject_name)
+                await ctx.send(f'{real_subject} already exists!')
+            except NameError:
+                await ctx.send('You can\'t add an \'all\' subject!')
     elif options.lower() == 'remove':
         if subject_name is None:
             await ctx.send('You need to specify what subject you want to remove!')
         else:
             try:
-                remove_subject(subject_name)
-                await ctx.send(f'The subject {subject_name} has been successfully removed')
+                real_subject = get_real_subject(subject_name)
+                remove_subject(real_subject)
+                await ctx.send(f'The subject {real_subject} has been successfully removed')
             except KeyError:
                 await ctx.send('That subject never existed!')
     else:
@@ -112,6 +118,12 @@ async def subjects(ctx, options=None, *, subject_name=None):
 async def homework(ctx, *, subject_name=None):
     if subject_name is None:
         await ctx.send('You need to specify what subject you want to check homework for!')
+    elif subject_name.lower() == 'all':
+        message = 'Homework for all subjects: '
+        for i in subject:
+            message = message + i + ": " + subject[i] + ", "
+        message = message[0:-2]
+        await ctx.send(message)
     else:
         try:
             await ctx.send(get_subject_homework(subject_name))
@@ -124,35 +136,48 @@ async def homework(ctx, *, subject_name=None):
 async def set_homework(ctx, *, subject_name=None, clear=None):
     if subject_name is None:
         await ctx.send('You need to specify a subject to set the homework to!')
-    elif not check_subject(subject_name):
+    try:
+        real_subject = get_real_subject(subject_name)
+    except KeyError:
         await ctx.send('That is not a valid subject!')
-    elif clear is not None:
+    if clear is not None:
         if clear.lower() == "clear":
             try:
-                set_subject_homework(subject_name, 'None')
-                await ctx.send(f'Successfully cleared the homework of {subject_name}')
+                set_subject_homework(real_subject, 'None')
+                await ctx.send(f'Successfully cleared the homework of {real_subject}')
             except KeyError:
                 await ctx.send('That is not a valid subject!')
         else:
             await ctx.send('Invalid clear argument!')
     else:
-        await ctx.send('What homework does that subject have?')
+        try:
+            await ctx.send('What homework does that subject have?')
 
-        def check(m):
-            return m.channel == ctx.channel
+            def check(m):
+                return m.channel == ctx.channel and m.author == ctx.author
 
-        msg = await client.wait_for('message', check=check, timeout=20.0)
-        set_subject_homework(subject_name, msg.content)
-        await ctx.send(f'Successfully set the homework of {subject_name} to {msg.content}')
+            msg = await client.wait_for('message', check=check, timeout=40.0)
+            if msg.content.lower() == 'clear':
+                set_subject_homework(real_subject, 'None')
+                await ctx.send(f'Successfully cleared the homework of {real_subject}')
+            else:
+                set_subject_homework(real_subject, msg.content)
+                await ctx.send(f'Successfully set the homework of {real_subject} to {msg.content}')
+        except TimeoutError:
+            await ctx.send('Homework timeout')
     await client.tree.sync()
 
 
 @client.hybrid_command(brief='Kill the bot')
 async def stop(ctx):
-    save_all()
-    await ctx.send('Shutting down')
-    await client.tree.sync()
-    await client.close()
+    if ctx.author.id == 434430979075997707:
+        save_all()
+        await ctx.send('Shutting down')
+        await client.tree.sync()
+        await client.close()
+    else:
+        await ctx.send('You think I\'d let anyone close the bot?')
+        await client.tree.sync()
 
 
 with open('token', 'r') as f:
